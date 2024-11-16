b3e2cc8ce67563202b2fff6407db58e43bd65be1
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-11-16 23:54:54 +0600

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+// 
+// FileName: ViewController.swift
+// ProjectName: SeaFood
+//
+// Created by MD ABIR HOSSAIN on 16-11-2024 at 11:54 PM
+// Website: https://mdabirhossain.com/
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
